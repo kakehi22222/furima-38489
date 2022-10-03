88678d1b7c9fa24aaf7a1119ea558681304f2601
kakehi22222 <kakehi22222@gmail.com>
2022-10-04 08:51:47 +0900

    itemテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+class Item < ApplicationRecord
+validates :goods, presence: true
+validates :information, presence: true
+validates :price, presence: true
+validates :category_id, presence: true
+validates :condition_id, presence: true
+validates :cost_burden_id, presence: true
+validates :prefectures_id, presence: true
+validates :aim_id
+
+belongs_to :user
+# has_one :purchase
+belongs_to_active_hash :category_id
+belongs_to_active_hash :condition_id
+belongs_to_active_hash :cost_burden_id
+belongs_to_active_hash :prefectures_id
+belongs_to_active_hash :aim_id
+
+end
---
+++ db/migrate/20221003233659_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :goods,null: false
+      t.text :information,null: false
+      t.integer :price,null: false
+      t.integer :category_id,null: false
+      t.integer :condition_id,null: false
+      t.integer :cost_burden_id,null: false
+      t.integer :prefectures_id,null: false
+      t.integer :aim_id,null: false, foreign_key: true
+      t.references :user,
+      t.timestamps
+    end
+  end
+end
