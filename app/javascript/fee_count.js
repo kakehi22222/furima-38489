99b1a54c03e18adc5f6ded82f2d9462bda88a1bd
kakehi22222 <kakehi22222@gmail.com>
2022-10-04 21:28:17 +0900

    不要なコード消去

--- app/javascript/fee_count.js
+++ app/javascript/fee_count.js
@@ -1,14 +1,13 @@
 function fee_count() {
 const itemPrice = document.getElementById("item-price");
 
-itemPrice.addEventListener('input',(e) => {
+itemPrice.addEventListener('input',() => {
   const countVal = itemPrice.value;
   const addTaxPrice = document.getElementById("add-tax-price");
   addTaxPrice.innerHTML = `${Math.floor(countVal/10)}`;
 
   const profit = document.getElementById("profit");
   profit.innerHTML = `${countVal - Math.floor(countVal/10)}`;
-  e.preventDefault();
 });
 };
 
